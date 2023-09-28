d0ac5e87e21afe39a6e6f4861e54f8832b75e407
IN <123793292+IN4111@users.noreply.github.com>
2023-09-28 16:51:21 +0530

    Delete SpamDetector directory

--- SpamDetector/App/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import AllRecords
-
-admin.site.register(AllRecords)
--- SpamDetector/App/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class AllRecords(models.Model):
-    message=models.TextField()
-    spam=models.BooleanField()
-    createdAt=models.DateTimeField(auto_now=True,editable=False)
